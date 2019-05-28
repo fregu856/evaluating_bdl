acf1901ab2e844359555567c20631d3c2b72b9f0
Fredrik Gustafsson <fregu856@gmail.com>
2019-05-28 13:40:22 +0200

    >

---
+++ toyClassification/Ensemble-MAP-Adam/datasets.py
@@ -0,0 +1,73 @@
+# code-checked
+
+import torch
+import torch.utils.data
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+import numpy as np
+
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+import pickle
+
+class ToyDataset(torch.utils.data.Dataset):
+    def __init__(self):
+        self.examples = []
+
+        with open("/root/evaluating_bdl/toyClassification/x.pkl", "rb") as file: # (needed for python3)
+            x = pickle.load(file) # (shape: (2000, 2))
+
+        with open("/root/evaluating_bdl/toyClassification/y.pkl", "rb") as file: # (needed for python3)
+            y = pickle.load(file) #  (shape: (2000, ))
+
+        x_1_train = []
+        x_2_train = []
+        y_train = []
+        for i in range(x.shape[0]):
+            if x[i, 0] > 0:
+                x_1_train.append(x[i, 0])
+                x_2_train.append(x[i, 1])
+                y_train.append(y[i])
+
+        y_train = np.array(y_train)
+        x_train = np.zeros((len(y_train), 2), dtype=np.float32)
+        x_train[:, 0] = np.array(x_1_train)
+        x_train[:, 1] = np.array(x_2_train)
+
+        x_train_false = x_train[y_train == 0] # (shape: (num_false, 2))
+        x_train_true = x_train[y_train == 1] # (shape: (num_true, 2))
+        print ("num_false: %d" % x_train_false.shape[0])
+        print ("num_true: %d" % x_train_true.shape[0])
+        plt.figure(1)
+        plt.plot(x_train_false[:, 0], x_train_false[:, 1], "r.")
+        plt.plot(x_train_true[:, 0], x_train_true[:, 1], "b.")
+        plt.ylabel("x_2")
+        plt.xlabel("x_1")
+        plt.xlim([-3, 3])
+        plt.ylim([-3, 3])
+        plt.savefig("/root/evaluating_bdl/toyClassification/Ensemble-MAP-Adam/training_data.png")
+        plt.close(1)
+
+        for i in range(x_train.shape[0]):
+            example = {}
+            example["x"] = x_train[i]
+            example["y"] = y_train[i]
+            self.examples.append(example)
+
+        self.num_examples = len(self.examples)
+
+    def __getitem__(self, index):
+        example = self.examples[index]
+
+        x = example["x"]
+        y = example["y"]
+
+        return (x, y)
+
+    def __len__(self):
+        return self.num_examples
+
+#_ = ToyDataset()
---
+++ toyClassification/Ensemble-MAP-Adam/eval_plots.py
@@ -0,0 +1,70 @@
+# code-checked
+
+from model import ToyNet
+
+import torch
+import torch.utils.data
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.optim as optim
+import torch.nn.functional as F
+
+import numpy as np
+import pickle
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import cv2
+
+import numpy as np
+
+x_min = -6.0
+x_max = 6.0
+num_points = 60
+
+M_values = [1, 4, 16, 64, 256]
+for M in M_values:
+    for iter in range(6):
+        network_inds = list(np.random.randint(low=0, high=1024, size=(M, )))
+        print (network_inds)
+
+        networks = []
+        for i in network_inds:
+            network = ToyNet("eval_Ensemble-MAP-Adam_1_M1024", project_dir="/root/evaluating_bdl/toyClassification").cuda()
+            network.load_state_dict(torch.load("/root/evaluating_bdl/toyClassification/training_logs/model_Ensemble-MAP-Adam_1_M1024_%d/checkpoints/model_Ensemble-MAP-Adam_1_M1024_epoch_150.pth" % i))
+            networks.append(network)
+
+        M_float = float(len(networks))
+        print (M_float)
+
+        for network in networks:
+            network.eval()
+
+        false_prob_values = np.zeros((num_points, num_points))
+        x_values = np.linspace(x_min, x_max, num_points, dtype=np.float32)
+        for x_1_i, x_1_value in enumerate(x_values):
+            for x_2_i, x_2_value in enumerate(x_values):
+                x = torch.from_numpy(np.array([x_1_value, x_2_value])).unsqueeze(0).cuda() # (shape: (1, 2))
+
+                mean_prob_vector = np.zeros((2, ))
+                for network in networks:
+                    logits = network(x) # (shape: (1, num_classes)) (num_classes==2)
+                    prob_vector = F.softmax(logits, dim=1) # (shape: (1, num_classes))
+
+                    prob_vector = prob_vector.data.cpu().numpy()[0] # (shape: (2, ))
+
+                    mean_prob_vector += prob_vector/M_float
+
+                false_prob_values[x_2_i, x_1_i] = mean_prob_vector[0]
+
+        plt.figure(1)
+        x_1, x_2 = np.meshgrid(x_values, x_values)
+        plt.pcolormesh(x_1, x_2, false_prob_values, cmap="RdBu", vmin=0, vmax=1)
+        plt.colorbar()
+        plt.tight_layout(pad=0.1, w_pad=0.1, h_pad=0.1)
+        plt.savefig("%s/predictive_density_M=%d_%d.png" % (network.model_dir, M, iter+1))
+        plt.savefig("%s/predictive_density_M=%d_%d.pdf" % (network.model_dir, M, iter+1), dpi=400)
+        plt.close(1)
+
+    print ("##################################################################")
