cf35881c96f5975897829abfdd89f32cb66ee891
Fredrik Gustafsson <fregu856@gmail.com>
2019-05-31 08:18:38 +0200

    >

---
+++ segmentation/models/aspp.py
@@ -0,0 +1,55 @@
+# code-checked
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, '../inplace_abn'))
+from bn import InPlaceABNSync
+
+class ASPP(nn.Module):
+    def __init__(self):
+        super(ASPP, self).__init__()
+
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+
+        self.conv_img = nn.Sequential(nn.Conv2d(4*512, 512, kernel_size=1, padding=0, dilation=1, bias=False),
+                                      InPlaceABNSync(512))
+
+        self.conv1 = nn.Sequential(nn.Conv2d(4*512, 512, kernel_size=1, padding=0, dilation=1, bias=False),
+                                   InPlaceABNSync(512))
+        self.conv2 = nn.Sequential(nn.Conv2d(4*512, 512, kernel_size=3, padding=12, dilation=12, bias=False),
+                                   InPlaceABNSync(512))
+        self.conv3 = nn.Sequential(nn.Conv2d(4*512, 512, kernel_size=3, padding=24, dilation=24, bias=False),
+                                   InPlaceABNSync(512))
+        self.conv4 = nn.Sequential(nn.Conv2d(4*512, 512, kernel_size=3, padding=36, dilation=36, bias=False),
+                                   InPlaceABNSync(512))
+
+        self.conv_bn_dropout = nn.Sequential(nn.Conv2d(5*512, 512, kernel_size=1, padding=0, dilation=1, bias=False),
+                                             InPlaceABNSync(512),
+                                             nn.Dropout2d(0.1))
+
+    def forward(self, feature_map):
+        # (feature_map has shape (batch_size, 4*512, h/8, w/8))
+
+        feature_map_h = feature_map.size()[2] # (== h/8)
+        feature_map_w = feature_map.size()[3] # (== w/8)
+
+        out_img = self.avg_pool(feature_map) # (shape: (batch_size, 4*512, 1, 1))
+        out_img = self.conv_img(out_img) # (shape: (batch_size, 512, 1, 1))
+        out_img = F.upsample(out_img, size=(feature_map_h, feature_map_w), mode="bilinear", align_corners=True) # (shape: (batch_size, 512, h/8, w/8))
+
+        out_1x1 = self.conv1(feature_map) # (shape: (batch_size, 512, h/8, w/8))
+        out_3x3_1 = self.conv2(feature_map) # (shape: (batch_size, 512, h/8, w/8))
+        out_3x3_2 = self.conv3(feature_map) # (shape: (batch_size, 512, h/8, w/8))
+        out_3x3_3 = self.conv4(feature_map) # (shape: (batch_size, 512, h/8, w/8))
+
+        out = torch.cat([out_img, out_1x1, out_3x3_1, out_3x3_2, out_3x3_3], 1) # (shape: (batch_size, 5*512, h/8, w/8))
+        out = self.conv_bn_dropout(out) # (shape: (batch_size, 512, h/8, w/8))
+
+        return out
---
+++ segmentation/models/model.py
@@ -0,0 +1,84 @@
+# code-checked
+
+import torch.nn as nn
+from torch.nn import functional as F
+import torch
+import os
+import sys
+import numpy as np
+from torch.autograd import Variable
+import functools
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, '../utils'))
+from resnet_block import conv3x3, Bottleneck
+
+from aspp import ASPP
+
+sys.path.append(os.path.join(BASE_DIR, '../inplace_abn'))
+from bn import InPlaceABNSync
+BatchNorm2d = functools.partial(InPlaceABNSync, activation='none')
+
+class ResNet(nn.Module):
+    def __init__(self, block, layers, num_classes):
+        print ("model.py")
+
+        self.inplanes = 128
+        super(ResNet, self).__init__()
+        self.conv1 = conv3x3(3, 64, stride=2)
+        self.bn1 = BatchNorm2d(64)
+        self.relu1 = nn.ReLU(inplace=False)
+        self.conv2 = conv3x3(64, 64)
+        self.bn2 = BatchNorm2d(64)
+        self.relu2 = nn.ReLU(inplace=False)
+        self.conv3 = conv3x3(64, 128)
+        self.bn3 = BatchNorm2d(128)
+        self.relu3 = nn.ReLU(inplace=False)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1, ceil_mode=True) # NOTE! (ceil_mode=True will do that x (batch_size, 128, h/4, w/4) e.g. has shape (batch_size, 128, 33, 33) instead of (batch_size, 128, 32, 32) if h == w == 256)
+
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=1, dilation=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=1, dilation=4, multi_grid=(1,1,1))
+
+        self.aspp = ASPP()
+
+        self.cls = nn.Conv2d(512, num_classes, kernel_size=1, stride=1, padding=0, bias=True)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilation=1, multi_grid=1):
+        downsample = None
+
+        if stride != 1 or self.inplanes != planes*block.expansion:
+            downsample = nn.Sequential(nn.Conv2d(self.inplanes, planes*block.expansion, kernel_size=1, stride=stride, bias=False),
+                                       BatchNorm2d(planes*block.expansion, affine=True))
+
+        layers = []
+        generate_multi_grid = lambda index, grids: grids[index%len(grids)] if isinstance(grids, tuple) else 1
+        layers.append(block(self.inplanes, planes, stride, dilation=dilation, downsample=downsample, multi_grid=generate_multi_grid(0, multi_grid)))
+        self.inplanes = planes*block.expansion
+
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes, dilation=dilation, multi_grid=generate_multi_grid(i, multi_grid)))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        # (x has shape: (batch_size, 3, h, w))
+
+        x = self.relu1(self.bn1(self.conv1(x))) # (shape: (batch_size, 64, h/2, w/2))
+        x = self.relu2(self.bn2(self.conv2(x))) # (shape: (batch_size, 64, h/2, w/2))
+        x = self.relu3(self.bn3(self.conv3(x))) # (shape: (batch_size, 128, h/2, w/2))
+        x = self.maxpool(x) # (shape: (batch_size, 128, h/4, w/4))
+        x = self.layer1(x) # (shape: (batch_size, 256, h/4, w/4))
+        x = self.layer2(x) # (shape: (batch_size, 512, h/8, w/8))
+        x = self.layer3(x) # (shape: (batch_size, 1024, h/8, w/8))
+        x = self.layer4(x) # (shape: (batch_size, 2048, h/8, w/8))
+        x = self.aspp(x) # (shape: (batch_size, 512, h/8, h/8))
+        x = self.cls(x) # (shape: (batch_size, num_classes, h/8, w/8))
+
+        return x
+
+def get_model(num_classes=19):
+    model = ResNet(Bottleneck,[3, 4, 23, 3], num_classes)
+    return model
