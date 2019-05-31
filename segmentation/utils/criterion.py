89cecb0dca41fc0af6306afc37af1dd0d23dbd36
Fredrik Gustafsson <fregu856@gmail.com>
2019-05-31 08:58:05 +0200

    >

---
+++ segmentation/utils/criterion.py
@@ -0,0 +1,20 @@
+# code-checked
+
+import torch.nn as nn
+import torch
+from torch.nn import functional as F
+
+class CriterionCrossEntropy(nn.Module):
+    def __init__(self, ignore_index=255):
+        super(CriterionCrossEntropy, self).__init__()
+        self.ignore_index = ignore_index
+        weight = torch.FloatTensor([0.8373, 0.918, 0.866, 1.0345, 1.0166, 0.9969, 0.9754, 1.0489, 0.8786, 1.0023, 0.9539, 0.9843, 1.1116, 0.9037, 1.0865, 1.0955, 1.0865, 1.1529, 1.0507])
+        self.criterion = torch.nn.CrossEntropyLoss(weight=weight, ignore_index=ignore_index)
+
+    def forward(self, preds, target):
+        h, w = target.size(1), target.size(2)
+        scale_pred = F.upsample(input=preds, size=(h, w), mode='bilinear', align_corners=True)
+
+        loss = self.criterion(scale_pred, target)
+
+        return loss
